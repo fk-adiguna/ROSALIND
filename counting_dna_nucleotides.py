a95698f7f6fee5072245270c6809d6177d431fc9
Fadil <fadil.k.adiguna@gmail.com>
2025-03-09 21:18:26 +0000

    changed output format to match ROSALIND answer

--- counting_dna_nucleotides.py
+++ counting_dna_nucleotides.py
@@ -6,15 +6,15 @@ random_dna_sequence = validate_sequence(generate_random_dna_sequence(50))
 def count_dna_nucleotides(sequence):
 
     count = {'A' : 0,
-             'T' : 0,
              'C' : 0,
              'G' : 0,
+             'T' : 0,
              }
 
     for nucleotide in sequence:
         count[nucleotide] += 1
     
-    return count
+    return ' '.join([str(count[nucleotide]) for nucleotide in count])
 
     # return dict(collections.Counter(sequence))
 
