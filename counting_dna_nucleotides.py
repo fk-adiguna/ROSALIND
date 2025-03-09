ffeda84f28eb3ffaf924d2d9d3c0c1478f9f6e86
Fadil <fadil.k.adiguna@gmail.com>
2025-03-09 21:14:22 +0000

    added collections

--- counting_dna_nucleotides.py
+++ counting_dna_nucleotides.py
@@ -1,4 +1,5 @@
 from dna_toolkit import *
+import collections
 
 random_dna_sequence = validate_sequence(generate_random_dna_sequence(50))
 
@@ -14,6 +15,8 @@ def count_dna_nucleotides(sequence):
         count[nucleotide] += 1
     
     return count
+
+    # return dict(collections.Counter(sequence))
 
 print(count_dna_nucleotides(random_dna_sequence))
 
