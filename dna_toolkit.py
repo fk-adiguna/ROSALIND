bec5d07cf5d1ff26f75553f796138ffd6f7ec886
Fadil <fadil.k.adiguna@gmail.com>
2025-03-09 21:54:09 +0000

    changed constant naming to full caps and added complement nucleotides dict

--- dna_toolkit.py
+++ dna_toolkit.py
@@ -1,16 +1,18 @@
 import random
 
-Nucleotides = ['A', 'C', 'G', 'T']
+NUCLEOTIDES = ['A', 'C', 'G', 'T']
+COMPLEMENT_NUCLEOTIDES = {'A' : 'T', 'T' : 'A', 'C' : 'G', 'G' : 'C'}
+
 
 # Checks sequence to make sure it is a DNA string
 def validate_sequence(dna_sequence):
     standard_sequence = dna_sequence.upper()
     for nucleotide in standard_sequence:
-        if nucleotide not in Nucleotides:
+        if nucleotide not in NUCLEOTIDES:
             return False
     return standard_sequence
 
 def generate_random_dna_sequence(length):
-    sequence = ''.join([random.choice(Nucleotides) for i in range(length + 1)])
+    sequence = ''.join([random.choice(NUCLEOTIDES) for i in range(length + 1)])
     return sequence
 
