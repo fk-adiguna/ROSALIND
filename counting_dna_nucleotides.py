4624d00743da8c99cca7c6285e862ca5485fc9e0
Fadil <fadil.k.adiguna@gmail.com>
2025-03-09 20:54:42 +0000

    edited

--- counting_dna_nucleotides.py
+++ counting_dna_nucleotides.py
@@ -1,6 +1,6 @@
 from dna_toolkit import *
+import random
 
-
-
-
+random_dna_sequence = ''.join([random.choice(Nucleotides) for nucleotide in range(20)])
+print(validate_sequence(random_dna_sequence))
 
